72f1b5af4c9bfc160340f51b30bf05e854df1cd5
Thomas Fink <53316058+ThomasAFink@users.noreply.github.com>
2023-11-26 19:42:28 +0100

    Add files via upload

--- logo_light.py
+++ logo_light.py
@@ -22,7 +22,7 @@ ax.set_xlim(-1.1, 1.1)
 ax.set_ylim(-1.1, 1.1)
 ax.set_aspect('equal')
 ax.axis('off')
-fig.patch.set_facecolor('#333333')
+fig.patch.set_facecolor('white')
 
 
 # Calculating radii for each part
@@ -31,21 +31,21 @@ inner_border_radius = outer_border_radius - border_width - lane_width
 center_line_radius = (outer_border_radius + inner_border_radius) / 2  # Centered between the two borders
 
 # Creating the outer border
-outer_border = plt.Circle((0, 0), outer_border_radius, color='white', fill=False, linewidth=border_width * 100)
+outer_border = plt.Circle((0, 0), outer_border_radius, color='#333333', fill=False, linewidth=border_width * 100)
 ax.add_artist(outer_border)
 
 # Creating the inner border
-inner_border = plt.Circle((0, 0), inner_border_radius, color='white', fill=False, linewidth=border_width * 100)
+inner_border = plt.Circle((0, 0), inner_border_radius, color='#333333', fill=False, linewidth=border_width * 100)
 ax.add_artist(inner_border)
 
 # Creating the lanes
-outer_lane = plt.Circle((0, 0), outer_border_radius - border_width / 1, color='#333333', fill=False, linewidth=lane_width * 100)
-inner_lane = plt.Circle((0, 0), inner_border_radius + border_width / 1, color='#333333', fill=False, linewidth=lane_width * 100)
+outer_lane = plt.Circle((0, 0), outer_border_radius - border_width / 1, color='white', fill=False, linewidth=lane_width * 100)
+inner_lane = plt.Circle((0, 0), inner_border_radius + border_width / 1, color='white', fill=False, linewidth=lane_width * 100)
 ax.add_artist(outer_lane)
 ax.add_artist(inner_lane)
 
 # Creating the center dashed line
-center_line = plt.Circle((0, 0), center_line_radius, color='white', fill=False, linewidth=center_line_width * 100, linestyle=(0, (5.55, 5.75)))
+center_line = plt.Circle((0, 0), center_line_radius, color='#333333', fill=False, linewidth=center_line_width * 100, linestyle=(0, (5.55, 5.75)))
 ax.add_artist(center_line)
 
 # Remove padding and margins
@@ -55,6 +55,6 @@ fig.subplots_adjust(left=0, right=1, top=1, bottom=0)
 
 
 # Saving the image in different resolutions
-plt.savefig('logo_icon_dark.jpg', dpi=100)  # 8K resolution
+plt.savefig('logo_icon_light.jpg', dpi=100)  # 8K resolution
 
 plt.show()
